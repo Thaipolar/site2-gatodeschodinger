eb31b1dd344f260f66d2d7836a6dcb9203f94b79
Thaipolar <81875428+Thaipolar@users.noreply.github.com>
2022-08-02 20:12:36 -0300

    Python

---
+++ ex003.py
@@ -0,0 +1,10 @@
+import time
+
+time.sleep(1)
+n1=int(input('Digite um número: '))
+time.sleep(1)
+n2=int(input('Digte outro número: '))
+
+resultado= n1+n2
+time.sleep(0.5)
+print('A soma de {} e {} é {}'.format(n1,n2,resultado))
